fa4b3aa27eb023c98f2b2e7c5e85df5afc528729
MENAPARA VIVEK <111960099+Vivekmen@users.noreply.github.com>
2024-04-18 13:59:57 +0530

    Update search crudapi_vivek.js

--- crudapi_vivek.js
+++ crudapi_vivek.js
@@ -1,7 +1,14 @@
 const http = require("http");
+const url = require("url");
 
 const data = [];
+const emailformat = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+const passwordformat =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/;
+
 const server = http.createServer((req, res) => {
+
+  const { pathname, query } = url.parse(req.url, true);
+
   if (req.method == "POST" && req.url === "/user") {
     let bodydata = "";
     req.on("data", (chunk) => {
@@ -9,15 +16,25 @@ const server = http.createServer((req, res) => {
     });
 
     req.on("end", () => {
-      const { username, password } = JSON.parse(bodydata);
+      const { username, email, password } = JSON.parse(bodydata);
       const findelement = data.some((e) => e.username === username);
 
       if (!findelement) {
         if (!username == "" && !password == "") {
-          data.push(JSON.parse(bodydata));
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.write(JSON.stringify({ msg: "User created Sucessfully" }));
-          res.end();
+          if (email.match(emailformat) && password.match(passwordformat)) {
+            data.push(JSON.parse(bodydata));
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.write(JSON.stringify({ msg: "User created Sucessfully" }));
+            res.end();
+          } else {
+            res.writeHead(409, { "Content-Type": "application/json" });
+            res.write(
+              JSON.stringify({
+                msg: "Please Enter valid format of Email or password",
+              })
+            );
+            res.end();
+          }
         } else {
           res.writeHead(404, { "Content-Type": "application/json" });
           res.write(
@@ -32,11 +49,28 @@ const server = http.createServer((req, res) => {
         res.end();
       }
     });
-  } else if (req.method == "GET" && req.url === "/user") {
+  } else if (req.method == "GET" && req.url === "/users") {
+
     res.writeHead(200, { "Content-Type": "application/json" });
     res.write(JSON.stringify(data));
     res.end();
+
+  } else if (req.method == "GET" &&pathname === "/user" &&query.username !== undefined) {
+
+    const { username, email, password } = query;
+    const findidxsearch = data.findIndex((e) => e.username === username);
+    if (findidxsearch != -1) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify(data[findidxsearch]));
+      res.end();
+    } else {
+      res.writeHead(409, { "Content-Type": "application/json" });
+      res.write(JSON.stringify({ msg: "User Is Not Exist!" }));
+      res.end();
+    }
+
   } else if (req.method == "PATCH" && req.url === "/user") {
+
     let bodydata = "";
     req.on("data", (chunk) => {
       bodydata += chunk;
@@ -56,8 +90,10 @@ const server = http.createServer((req, res) => {
         res.write(JSON.stringify({ msg: "User Is Not Exist!" }));
         res.end();
       }
+
     });
   } else if (req.method == "DELETE" && req.url === "/user") {
+
     let bodydata = "";
     req.on("data", (chunk) => {
       bodydata += chunk;
@@ -78,7 +114,10 @@ const server = http.createServer((req, res) => {
         res.end();
       }
     });
-  } else {
+
+  } 
+  else
+   {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.write(JSON.stringify({ msg: "Page Not Found!" }));
     res.end();
